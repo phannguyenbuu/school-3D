189c2d35cfdcdf410410d042a75214aed4080dab
phannguyenbuu <phannguyenbuu@gmail.com>
2025-10-06 18:15:55 +0700

    Deloy 5

--- src/admin/app.py
+++ src/admin/app.py
@@ -84,7 +84,8 @@ def add_header(response):
 @app.route('/')
 @app.route('/<path:path>')
 def index(path=None):
-    current_path = request.path  # Lấy đường dẫn URL hiện tại
+    current_path = request.path  # Lấy đường dẫn URL hiện 
+    print('path', current_path)
 
     if 'cis' in current_path:
         html = 'index_cis.html'
