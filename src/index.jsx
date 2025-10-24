e941f15b4f1278127e1ae32e8d1a6fe111d01223
phannguyenbuu <phannguyenbuu@gmail.com>
2025-10-24 08:18:29 +0700

    J12

--- src/index.jsx
+++ src/index.jsx
@@ -45,7 +45,7 @@ function RootApp() {
   
 
   const [curZ, setCurZ] = useState(-1);
-  const isSEDSchool = window.location.href.includes("sed") ? 0 : 1;
+  const isSEDSchool = window.location.href.includes("sed") ? 1 : 0;
   const default_school_routes = isSEDSchool ? school_1_routes: school_2_routes;
   const default_school_data = isSEDSchool ? school_1_data: school_2_data;
   const [regions, setRegions] = useState(default_school_data);
