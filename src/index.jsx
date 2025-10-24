a80e617e64fea92c0fd4cc597f384d1c2f3bc835
phannguyenbuu <phannguyenbuu@gmail.com>
2025-10-24 08:15:57 +0700

    J11

--- src/index.jsx
+++ src/index.jsx
@@ -45,7 +45,7 @@ function RootApp() {
   
 
   const [curZ, setCurZ] = useState(-1);
-  const isSEDSchool = process.env.REACT_APP_ISSEQ === "1";
+  const isSEDSchool = window.location.href.includes("sed") ? 0 : 1;
   const default_school_routes = isSEDSchool ? school_1_routes: school_2_routes;
   const default_school_data = isSEDSchool ? school_1_data: school_2_data;
   const [regions, setRegions] = useState(default_school_data);
@@ -70,8 +70,7 @@ function RootApp() {
   },[]);
 
 
-  document.title = isSEDSchool ? '3D Map - Sedbergh Vietnam':'3D Map - CIS';
-  changeFavicon(isSEDSchool ? "/images/LogoSedberghVietnam.png" : "/images/LogoCIS.png");
+  
 
   const navigate = useNavigate();
 
@@ -116,6 +115,8 @@ function RootApp() {
 
   useEffect(() => {
     setClr(schoolIndex === 0 ? '#ab9572':'#7c1819');
+    document.title = schoolIndex ? '3D Map - Sedbergh Vietnam':'3D Map - CIS';
+    changeFavicon(schoolIndex ? "/images/LogoSedberghVietnam.png" : "/images/LogoCIS.png");
   }, [schoolIndex]);
 
   return (
